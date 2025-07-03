26baddbd66d31a0feedc5e2cd4bb2c0980ce9168
THYWIZARD <abdulhaqabdulganiy@gmail.com>
2025-07-03 08:09:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function openTab(evt, tabName) {
     var tabcontent = document.getElementsByClassName("tab-content");
     for (var i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].classList.remove("active");
+        tabcontent[i].style.display = "none";
     }
     var tablinks = document.getElementsByClassName("tab-button");
     for (var i = 0; i < tablinks.length; i++) {
         tablinks[i].classList.remove("active");
     }
-    document.getElementById(tabName).classList.add("active");
+    document.getElementById(tabName).style.display = "block";
     evt.currentTarget.classList.add("active");
 }
